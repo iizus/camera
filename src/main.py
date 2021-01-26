28cde442ed611884827057dd48ac89e96414460d
iizus <shota.iizuka@icloud.com>
2021-01-26 22:51:05 +0900

    remove extra code

--- src/main.py
+++ src/main.py
@@ -2,16 +2,6 @@ from capture import Capture
 
 video_source = -1
 camera = Capture(video_source)
-
-camera.take_and_save_with_timestamp()
-
-camera.display_settings()
-# camera.take_and_save_to('test1.png')
-
-# camera.codec = 'MJPG'
-# camera.fps = 30
-# camera.set_frame_size(width=1920, height=1080)
-
 camera.set_frame(
     codec = 'MJPG',
     width = 1920,
@@ -19,17 +9,6 @@ camera.set_frame(
     fps = 30,
 )
 camera.display_settings()
-# camera.take_and_save_to('test2.png')
-
-# camera.codec = 'YUYV'
-# camera.rgb = 0
-
-camera.set_frame(
-    codec = 'YUYV',
-    width = 1920,
-    height = 1080,
-    fps = 30,
-)
-camera.display_settings()
+camera.take_and_save_with_timestamp()
 
 del camera
